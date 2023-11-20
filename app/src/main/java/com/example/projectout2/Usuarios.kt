7d8c3ea06871c51f0c2f7e0986727b311effd7bc
ricardodr13 <ricardodr13@gmail.com>
2023-11-20 16:12:05 +0100

    Creacion clase Usuarios.kt

---
+++ app/src/main/java/com/example/projectout2/Usuarios.kt
@@ -0,0 +1,16 @@
+package com.example.projectout2
+
+/*  nombre: nombre en la red social
+    apellido: usuario
+    email: correo de la cuenta del usuario, usado para registrarse y logearse
+    password: Contraseña que el usuario ha establecido para iniciar sesion
+    fechaNacimiento: Dato para el perfil.
+
+*/
+data class Usuarios(
+    val nombre: String,
+    val apellido: String,
+    val password: String,
+    val email: String,
+    val fechaNacimiento: Int
+)
