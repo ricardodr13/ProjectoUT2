f793573bc591388e089f5f7783dd6073b1787bc8
ricardodr13 <ricardodr13@gmail.com>
2023-11-22 17:48:42 +0100

    Creacion Panatalla Login.kt, la imagen debe ser de 250x250 pixeles.

---
+++ app/src/main/java/com/example/projectout2/Login.kt
@@ -0,0 +1,137 @@
+package com.example.projectout2
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.projectout2.ui.theme.ProjectoUT2Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login() {
+    var user by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var login by remember { mutableStateOf(false) }
+    var boton by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .width(250.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.Titulo_app),
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        Card( modifier = Modifier
+            .height(250.dp)
+            .width(250.dp)
+        ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "imagen",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+        )
+        }
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+        ) {
+
+            Text(
+                text = stringResource(id = R.string.TextField_Email),
+                Modifier.padding(top= 32.dp),
+                style = MaterialTheme.typography.labelMedium
+            )
+
+            OutlinedTextField(
+                value = user,
+                onValueChange = { user = it },
+                label = { Text(stringResource(id = R.string.TextField_Email)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            Text(
+                text = stringResource(id = R.string.TextField_Password),
+                Modifier.padding(top= 16.dp),
+                style = MaterialTheme.typography.labelMedium
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(stringResource(id = R.string.TextField_Password)) },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)) {
+            Button(
+                onClick = {
+                    // implementar NavHost -> HomeScreen
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text("Log in")
+            }
+            if (login) {
+                Text(
+                    text = "Usuario y contraseña incorrectos.",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 50.dp),
+                    color = Color.Red,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+//Preview de la pantalla login
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    ProjectoUT2Theme {
+        Login()
+    }
+}
--- app/src/main/java/com/example/projectout2/ui/theme/Login.kt
+++
@@ -1,5 +0,0 @@
-package com.example.projectout2.ui.theme
-
-fun Login() {
-
-}
