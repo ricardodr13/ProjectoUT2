cbfd4c163333af6c294b1d7b3392a217dd640c1b
Ellie Colorado <123810583+Elrecoal@users.noreply.github.com>
2023-11-23 19:58:29 +0100

    Cambios: HomeScreen, Publicacion, Usuario
    -HomeScreen: Se ha eliminado la lista de usuarios y publicaciones que se creaba en el interior y, en su lugar, se pide la lista de publicaciones (con sus usuarios ya introducidos) como argumento.
    
    -Usuario: Se ha creado la funcion "generarListaUsuarios" para obtener una lista de usuarios facilmente. Actualmente tiene tres usuarios especificos, aunque se podría modificar para crear listas, de usuarios genéricos, más largas
    
    -Publicacion: Se ha creado la funcion "generarListaPublicaciones" para obtener una lista de publicaciones a partir de una lista de usuarios que pide como argumento. Genera dos publicaciones por usuario.

--- app/src/main/java/com/example/projectout2/Usuario.kt
+++ app/src/main/java/com/example/projectout2/Usuario.kt
@@ -1,11 +1,12 @@
 package com.example.projectout2
 
-/*  nombre: nombre en la red social
+/*
+    nombre: nombre en la red social
     apellido: usuario
     email: correo de la cuenta del usuario, usado para registrarse y logearse
     password: Contraseña que el usuario ha establecido para iniciar sesion
     fechaNacimiento: Dato para el perfil.
-
+    fotoPerfil: Id de la foto usada para la foto de perfil en Mi perfil, Busca e Inicio
 */
 data class Usuario(
 
@@ -16,4 +17,13 @@ data class Usuario(
     val fechaNacimiento: Int,
     val fotoPerfil: Int
 
-    )
+)
+//Funcion para generar tres usuarios determinados
+fun generarListaUsuarios(): List<Usuario> {
+
+    val u1 = Usuario("Ellie", "Apellido1", "contraseña1", "email1", 1, R.drawable.foto)
+    val u2 = Usuario("Pepe", "Apellido2", "contraseña2", "email2", 2, R.drawable.foto)
+    val u3 = Usuario("Ricardo", "Apellido3", "contraseña3", "email3", 3, R.drawable.foto)
+    return listOf(u1,u2,u3)
+
+}
